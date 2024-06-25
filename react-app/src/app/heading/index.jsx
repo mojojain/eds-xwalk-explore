8e37d5c7818e9a9395742971413aa9a7873286c3
manojlily <manojjain@adobe.com>
2024-06-25 11:22:29 +0530

    fixed react block

--- react-app/src/app/heading/index.jsx
+++ react-app/src/app/heading/index.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import './index.css';
 
-const elem = document.querySelector('.heading-react-container');
+const elem = document.querySelector('.heading-react');
 if (elem) {
   const root = createRoot(elem);
   root.render(<App />);
