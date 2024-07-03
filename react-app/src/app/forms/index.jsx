de53f46df4d77756d7c5926fc6f7b84935bd6481
manojlily <manojjain@adobe.com>
2024-07-03 22:55:03 +0530

    cd

--- react-app/src/app/forms/index.jsx
+++ react-app/src/app/forms/index.jsx
@@ -5,7 +5,7 @@ import './index.css';
 
 const elem = document.querySelector('.form');
 if (elem) {
-  const formURL = elem.querySelector(".button-container a").href;
+  const formURL = elem.querySelector("form").dataset.action;
   const root = createRoot(elem);
   root.render(<App formURL={formURL} />);
 }
